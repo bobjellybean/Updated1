d32e5b1bff604ec0d28aef82b3b2d3b5783d32e8
bobjellybean <69946226+bobjellybean@users.noreply.github.com>
2021-04-06 11:44:23 +0530

    Add files via upload

---
+++ owngame4-main/bullets.js
@@ -0,0 +1,6 @@
+class Bullet extends baseclass {
+    constructor (x,y) {
+        super(x,y,50,50);
+        this.image=loadImage("Images/bullet1.png");
+    }
+}
---
+++ owngame4-main/mellon.js
@@ -0,0 +1,10 @@
+class mellon extends baseclass {
+    constructor (x,y) {
+        super(x,y,50,50);
+        this.image = loadImage("Images/melon.png");
+    }
+    display(){
+        super.display();
+
+    }
+}
